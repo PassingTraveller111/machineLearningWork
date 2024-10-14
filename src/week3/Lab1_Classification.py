30a38a27612f285d61ec48af4e0766dc3e2ecb7c
liujingmin <1220839184@qq.com>
2024-10-14 18:55:19 +0800

    分类、激活函数和决策边界的实验代码

---
+++ src/week3/Lab1_Classification.py
@@ -0,0 +1,47 @@
+import numpy as np
+# %matplotlib widget
+import matplotlib.pyplot as plt
+from lab_utils_common import dlc, plot_data
+from plt_one_addpt_onclick import plt_one_addpt_onclick
+plt.style.use('./deeplearning.mplstyle')
+
+
+# 本节讲到的都是二元分类，即只有是或者否两种输出的分类方式
+# 下面是两个二元分类的例子，分别有一和二个特征
+# 下面的图中y的值只能是0或1
+# 下面的例子表明，线性模型不足以对分类数据进行建模。
+x_train = np.array([0., 1, 2, 3, 4, 5])
+y_train = np.array([0,  0, 0, 1, 1, 1])
+X_train2 = np.array([[0.5, 1.5], [1,1], [1.5, 0.5], [3, 0.5], [2, 2], [1, 2.5]])
+y_train2 = np.array([0, 0, 0, 1, 1, 1])
+
+pos = y_train == 1
+neg = y_train == 0
+
+fig,ax = plt.subplots(1,2,figsize=(8,3))
+#plot 1, single variable
+ax[0].scatter(x_train[pos], y_train[pos], marker='x', s=80, c = 'red', label="y=1")
+ax[0].scatter(x_train[neg], y_train[neg], marker='o', s=100, label="y=0", facecolors='none',
+              edgecolors=dlc["dlblue"],lw=3)
+
+ax[0].set_ylim(-0.08,1.1)
+ax[0].set_ylabel('y', fontsize=12)
+ax[0].set_xlabel('x', fontsize=12)
+ax[0].set_title('one variable plot')
+ax[0].legend()
+
+#plot 2, two variables
+plot_data(X_train2, y_train2, ax[1])
+ax[1].axis([0, 4, 0, 4])
+ax[1].set_ylabel('$x_1$', fontsize=12)
+ax[1].set_xlabel('$x_0$', fontsize=12)
+ax[1].set_title('two variable plot')
+ax[1].legend()
+plt.tight_layout()
+plt.show()
+
+
+w_in = np.zeros((1))
+b_in = 0
+plt.close('all')
+addpt = plt_one_addpt_onclick( x_train,y_train, w_in, b_in, logistic=False)
---
+++ src/week3/Lab2_Sigmoid_function.py
@@ -0,0 +1,73 @@
+import numpy as np
+# %matplotlib widget
+import matplotlib.pyplot as plt
+from plt_one_addpt_onclick import plt_one_addpt_onclick
+from lab_utils_common import draw_vthresh
+plt.style.use('./deeplearning.mplstyle')
+
+# Input is an array.
+'''
+np.exp 用于计算输入数组中每个元素，以e为底数，元素为指数的数值，即e^i,i为元素。
+np.exp(arr)，其中arr是输入的数组或数值。
+import numpy as np
+
+arr = np.array([1, 2, 3])
+print(np.exp(arr))  # [ 2.71828183  7.3890561  20.08553692]
+
+single_value = 5
+print(np.exp(single_value))  # 148.4131591025766
+'''
+input_array = np.array([1,2,3])
+exp_array = np.exp(input_array)
+
+print("Input to exp:", input_array)
+print("Output of exp:", exp_array)
+
+# Input is a single number
+input_val = 1
+exp_val = np.exp(input_val)
+
+print("Input to exp:", input_val)
+print("Output of exp:", exp_val)
+
+# 下面是激活函数的实现
+def sigmoid(z):
+    """
+    Compute the sigmoid of z
+
+    Args:
+        z (ndarray): A scalar, numpy array of any size.
+
+    Returns:
+        g (ndarray): sigmoid(z), with the same shape as z
+
+    """
+    # 激活函数就需要依赖与np.exp的计算
+    g = 1 / (1 + np.exp(-z))
+
+    return g
+
+# 生成从-10到10的数
+# Generate an array of evenly spaced values between -10 and 10
+z_tmp = np.arange(-10,11)
+# 用激活函数依次生成从-10到10的激活函数的输出值
+# Use the function implemented above to get the sigmoid values
+y = sigmoid(z_tmp)
+
+# Code for pretty printing the two arrays next to each other
+np.set_printoptions(precision=3)
+print("Input (z), Output (sigmoid(z))")
+# 将z_temp和y按列拼接起来
+print(np.c_[z_tmp, y])
+
+# 下面绘制了激活函数g的值关于z的图像
+# 当z变大为负值时，sigmoid函数趋近于0，当z增大为正值时，sigmoid函数趋近于1
+# Plot z vs sigmoid(z)
+fig,ax = plt.subplots(1,1,figsize=(5,3))
+ax.plot(z_tmp, y, c="b")
+
+ax.set_title("Sigmoid function")
+ax.set_ylabel('sigmoid(z)')
+ax.set_xlabel('z')
+draw_vthresh(ax,0)
+plt.show()
